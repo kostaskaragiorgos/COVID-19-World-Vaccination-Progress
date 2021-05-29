d0d3f62df31da665b6c9019b1c6c45c0770cba93
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-05-29 16:05:16 +0300

    Update dailywwvacinationreport.py

--- dailywwvacinationreport.py
+++ dailywwvacinationreport.py
@@ -2,3 +2,5 @@ import pandas as pd
 def createdataframe(filename):
     return pd.read_csv(filename)
 
+def cleardataframe(dataframe):
+    return dataframe.drop_duplicates(subset='location', keep='last', inplace=True)
