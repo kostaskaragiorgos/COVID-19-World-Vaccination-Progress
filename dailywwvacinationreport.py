99473f666f09a29255c1b84227228d57cdb1c8be
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-05-29 16:38:34 +0300

    Update dailywwvacinationreport.py

--- dailywwvacinationreport.py
+++ dailywwvacinationreport.py
@@ -4,3 +4,9 @@ def createdataframe(filename):
 
 def cleardataframe(dataframe):
     return dataframe.drop_duplicates(subset='location', keep='last', inplace=True)
+
+def removecontinents(dataframe):
+    indexlist = ["Asia", "Europe", "Africa", "Middle East", "World", "Upper middle income", "High income","North America", "Lower middle income"]
+    for i in indexlist:
+        dataframe.drop(dataframe[dataframe['location']== str(i)].index, inplace=True)
+    return dataframe
