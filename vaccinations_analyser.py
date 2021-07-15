8c4658d0d1f8a518d0061ad6199a75bd7e4e6849
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-07-15 09:49:35 +0300

    Update vaccinations_analyser.py

--- vaccinations_analyser.py
+++ vaccinations_analyser.py
@@ -81,6 +81,10 @@ class Vaccinations_Analyser():
         self.master.bind('<Control-i>',lambda event: aboutmenu())
     
     def plot_vaccination(self, fully=False):
+        """plots the number of vaccinations total/fully
+        Args:
+            fully: a flag that determines which dataset column to use
+        """
         if self.filename == "":
             msg.showerror("ERROR", "NO FILE IMPORTED")
         else:
